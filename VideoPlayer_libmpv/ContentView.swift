ca488136598f95303dec55f6a4a9a92f5c4b0050
LexTheAries2209 <673661658@qq.com>
2024-01-25 15:19:58 +0800

    Initial Commit

---
+++ VideoPlayer_libmpv/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VideoPlayer_libmpv
+//
+//  Created by 吴坤城 on 1/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VideoPlayer_libmpv/VideoPlayer_libmpvApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayer_libmpvApp.swift
+//  VideoPlayer_libmpv
+//
+//  Created by 吴坤城 on 1/25/24.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayer_libmpvApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
